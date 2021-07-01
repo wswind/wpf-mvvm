898df522395eb7308cee7e4e33aab3c4dce1761d
ws <ws_dev@163.com>
2021-07-01 14:00:22 +0800

    init

---
+++ WpfApp1/WpfApp1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            HelloText = "Hello WPF MVVM";
+        }
+        public string HelloText { get; set; }
+    }
+}
